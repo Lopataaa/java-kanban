36caf4815930b67cf7d82e8d39d039b38d25cb1d
User Oksana <oks321mih@yandex.ru>
2025-08-31 13:10:51 +0300

    Удалила лишний импорт после Checkout test на GitHub

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Set;
 
 import exception.ManagerSaveException;
 import task.SubTask;
